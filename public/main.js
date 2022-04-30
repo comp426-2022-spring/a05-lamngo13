561fe76891b86b9e6ab7683ac870d7226f5b41ca
lamngo13 <lamngo@live.unc.edu>
2022-04-30 17:19:22 -0400

    somethings upski with the api rip

--- public/main.js
+++ public/main.js
@@ -1,5 +1,19 @@
 // Focus div based on nav button click
+const coin = document.getElementById('coin');
 
+coin.addEventListener("click", flipCoin);
+
+//big boy
+async function flipCoin() {
+    const endpoint = "app/flip/";
+    const url = document.baseURI+endpoint;
+    //PI of A (API for u cultured folks)
+    await fetch(url);
+                        .then(function(response) {
+                        return response.json();    
+                        })
+}
+//end big boy
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
