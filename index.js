c740e76ba37797dfb9dcb4ea244e5b78db5b5627
lamngo13 <lamngo@live.unc.edu>
2022-04-30 15:12:51 -0400

    cleaned up my comments for the coin functions

--- index.js
+++ index.js
@@ -74,28 +74,6 @@ function coinFlip() {
   }
   }
   
-  
-  
-  
-  /** Multiple coin flips
-   * 
-   * Write a function that accepts one parameter (number of flips) and returns an array of 
-   * resulting "heads" or "tails".
-   * 
-   * @param {number} flips 
-   * @returns {string[]} results
-   * 
-   * example: coinFlips(10)
-   * returns:
-   *  [
-        'heads', 'heads',
-        'heads', 'tails',
-        'heads', 'tails',
-        'tails', 'heads',
-        'tails', 'heads'
-      ]
-   */
-  
   function coinFlips(flips) {
     let theResult = ""
     let bruh = [];
@@ -106,19 +84,6 @@ function coinFlip() {
     }
   return bruh;
   }
-  
-  /** Count multiple flips
-   * 
-   * Write a function that accepts an array consisting of "heads" or "tails" 
-   * (e.g. the results of your `coinFlips()` function) and counts each, returning 
-   * an object containing the number of each.
-   * 
-   * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
-   * { tails: 5, heads: 5 }
-   * 
-   * @param {string[]} array 
-   * @returns {{ heads: number, tails: number }}
-   */
   
   function countFlips(array) {
     let nHeads = 0;
@@ -141,34 +106,9 @@ function coinFlip() {
       }
     })
     return returnable
-  
-  
-    //if (nHeads == 0) {
-    //  return "{ tails: " + nTails.toString() + " }";
-    //}
-    //else if (nTails == 0){
-    //  return "{ heads: " + nHeads.toString() + " }";
-    //}
-    //else {
-    //  //return "{ heads: " + nHeads + " tails: " + nTails + " }"
-    //  return "{heads: " + nHeads + " tails: " + nTails + "}"
-    //  
-    //}
-  
-    //return array;
+
   }
-  
-  /** Flip a coin!
-   * 
-   * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
-   * 
-   * @param {string} call 
-   * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
-   * 
-   * example: flipACoin('tails')
-   * returns: { call: 'tails', flip: 'heads', result: 'lose' }
-   */
-  
+
   function flipACoin(call) {
       let flip = coinFlip()
       const obj = { call: call, flip: flip, result: 'lose' }
@@ -176,13 +116,6 @@ function coinFlip() {
           obj.result = 'win';
       }
       return obj;
-    //let result = coinFlip();
-    //let winner = ""
-    //if (call == result) {
-    //  winner = "win"
-    //} else {
-    //  winner = "lose"
-   // }
-  //  return "{call:" + call +",flip:" + result + ",result:" + winner +"}"
+
   }
 //END COIN FUNCTIONS
