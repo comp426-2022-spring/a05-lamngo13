ab09b1672b718a3ac6647af6f0f67db5bc5cd783
lamngo13 <lamngo@live.unc.edu>
2022-04-30 17:33:21 -0400

    working on main.js

--- public/main.js
+++ public/main.js
@@ -20,3 +20,32 @@ async function flipCoin() {
                             });
 };
 
+const coins = document.getElementById("coins")
+coins.addEventListener("submit", flipCoins)
+
+//big chonker
+async function flipCoins(event) {
+    //this is cool I didn't know this!
+    //we are using an event, and thus need to remove
+    // the default browswer event - a reload!
+    event.preventDefault();
+    const endpoint = "app/flip/coins/"
+    const url = document.baseURI+endpoint
+    const formEvent = event.currentTarger
+
+    //this can fail so we want to TRY
+    try {
+            const formData = new FormData(formEvent);
+            const flips = await sendFlips({url, formData});
+            console.log(flips);
+            document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
+		    document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
+        document.getElementById("coinlist").innerHTML = coinList(flips.raw);
+    } catch (error) {
+        console.log(error);
+    }
+    //still inside the chonker
+
+    }
+//end big chonker
+
